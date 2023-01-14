742ad0c043f48ad5d164841e2d4ae5ce9eddfdbf
billmills <you@example.com>
2023-01-13 19:50:17 -0500

    drafting argovis document construction

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ import numpy as np
 from TransMat import TransMat
 from folium.plugins import HeatMap
 
+from pymongo import MongoClient
+import util.helpers as h
+
 class ExcelFloat(Core):
 	@classmethod
 	def from_excel(cls,filename):
@@ -35,41 +38,67 @@ class ExcelFloat(Core):
 		return (cls(holder_array,trans_geo=trans_mat.trans_geo),float_lats,float_lons)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("filename", help="filename of csv file with argo locations",
-                    type=str)
-parser.add_argument("timestep", help="timestep of prediction",
-                    type=float)
+parser.add_argument("filename", help="filename of csv file with argo locations", type=str)
+#parser.add_argument("timestep", help="timestep of prediction",type=float)
 args = parser.parse_args()
-assert args.timestep>45, "timestep must be greater than 45 days"
-timestep = round(args.timestep/90)
-if args.timestep-timestep*90>45:
-	timestep += 1
-float_array,float_lats,float_lons = ExcelFloat.from_excel(args.filename)
-trans_mat = TransMat.load(os.path.join(ROOT_DIR,'90-2-2.npy'),GeoClass=TransitionGeo)
-trans_calc = trans_mat.multiply(timestep-1)
+records = {} # will be a dictionary of records for this origin lon/lat keyed by (forecast lon, forecast lat)
+timesteps = list(range(90,181,90))
+timesteps.reverse() # start with the longest forecast so we start with the complete set of forecast long/lats
+for timestep in timesteps:
+	#assert args.timestep>45, "timestep must be greater than 45 days"
+	timestep = round(timestep/90)
+	#if args.timestep-timestep*90>45:
+	#	timestep += 1
+	float_array,float_lats,float_lons = ExcelFloat.from_excel(args.filename)
+	trans_mat = TransMat.load(os.path.join(ROOT_DIR,'90-2-2.npy'),GeoClass=TransitionGeo)
+	trans_calc = trans_mat.multiply(timestep-1)
 
-output = trans_calc.todense().dot(float_array.todense())
-output = [x[0] for x in output.tolist()]
-lats,lons = zip(*trans_mat.trans_geo.total_list.tuple_total_list())
-df = pd.DataFrame({'Latitude':lats,'Longitude':lons,'Probability':output})
-df = df[df.Probability!=0]
-df = df.reset_index(drop=True)
+	output = trans_calc.todense().dot(float_array.todense())
+	output = [x[0] for x in output.tolist()]
+	lats,lons = zip(*trans_mat.trans_geo.total_list.tuple_total_list())
+	df = pd.DataFrame({'Latitude':lats,'Longitude':lons,'Probability':output})
+	df = df[df.Probability!=0]
+	df = df.reset_index(drop=True)
 
-url = 'https://server.arcgisonline.com/arcgis/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}'
-map=folium.Map(location=[0,0],zoom_start=2)
-folium.TileLayer(tiles=url, attr='World_Ocean_Base').add_to(map)
-for x,y in zip(float_lons,float_lats):
-	folium.Marker([y,x],
-	              popup='Float Located at %s , %s'%(y,x),
-	              icon=folium.Icon(color='green')
-	             ).add_to(map)	
-HeatMap(df, 
-        min_opacity=0.4,
-        blur = 18
-               ).add_to(folium.FeatureGroup(name='Heat Map').add_to(map))
-folium.LayerControl().add_to(map)
+	if not records:
+		for index, row in df.iterrows():
+			records[(row['Longitude'], row['Latitude'])] = [[None]]*len(timesteps)
+	for index, row in df.iterrows():
+			records[(row['Longitude'], row['Latitude'])][timestep - 1] = [row['Probability']]
+
+for key, value in records.items():
+	doc = {
+		"_id": str(h.tidylon(float_lons[0])) + '_' + str(float_lats[0]) + '_' + str(h.tidylon(key[0])) + '_' + str(key[1]),
+		"metadata": ['covariance'],
+		"geolocation": {"type":"Point", "coordinates":[h.tidylon(float_lons[0]),float_lats[0]]},
+		"geolocation_forecast": {"type":"Point", "coordinates":[h.tidylon(key[0]),key[1]]},
+		"data": value
+	}
+	print(doc)
+
+	# url = 'https://server.arcgisonline.com/arcgis/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}'
+	# map=folium.Map(location=[0,0],zoom_start=2)
+	# folium.TileLayer(tiles=url, attr='World_Ocean_Base').add_to(map)
+	# for x,y in zip(float_lons,float_lats):
+	# 	folium.Marker([y,x],
+	# 	              popup='Float Located at %s , %s'%(y,x),
+	# 	              icon=folium.Icon(color='green')
+	# 	             ).add_to(map)	
+	# HeatMap(df, 
+	#         min_opacity=0.4,
+	#         blur = 18
+	#                ).add_to(folium.FeatureGroup(name='Heat Map').add_to(map))
+	# folium.LayerControl().add_to(map)
+
+	# df.to_csv(os.path.join(ROOT_DIR,'Output/'+args.filename+'_tm_output.csv'))
+	# map.save(outfile=os.path.join(ROOT_DIR,'./Output/map.html'))
+	# os.system('open '+os.path.join(ROOT_DIR,'./Output/map.html'))
 
 
-df.to_csv(os.path.join(ROOT_DIR,'Output/'+args.filename+'_tm_output.csv'))
-map.save(outfile=os.path.join(ROOT_DIR,'./Output/map.html'))
-os.system('open '+os.path.join(ROOT_DIR,'./Output/map.html'))
+
+
+# print(df.iloc[0]['Latitude'])
+# print(df.iloc[0]['Longitude'])
+# print(df.iloc[0]['Probability'])
+# print(float_lats,float_lons)
+# print(float_array)
